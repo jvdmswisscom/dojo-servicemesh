0a8ee664d16e9603183c9e817e7b330786cfff37
Jacques van der Merwe <jacques.vandermerwe@swisscom.com>
2019-05-08 08:57:55 +0200

    consul service discovery

--- services/users/entrypoint.sh
+++ services/users/entrypoint.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 
 echo "Waiting for postgres..."
 
@@ -8,6 +9,29 @@ done
 
 echo "PostgreSQL started"
 
+# consul service registration configuration
+IPADDR=`ip a show eth0 | grep -oP 'inet \K[\d\.]+'`
+
+cat <<EOF | tee /etc/consul.d/${CONSUL_SERVICE}.json
+{
+  "service": {
+    "name": "${CONSUL_SERVICE}",
+    "port": 5000
+  }
+}
+EOF
+
+# start consul agent
+nohup consul agent \
+	-bind=${IPADDR} \
+	-retry-join=consul \
+	--data-dir=/var/consul \
+	-config-dir=/etc/consul.d \
+	&
+
+sleep 2
+
+# testdriven service 
 python manage.py recreate-db
 python manage.py seed-db
 gunicorn -b 0.0.0.0:5000 manage:app
