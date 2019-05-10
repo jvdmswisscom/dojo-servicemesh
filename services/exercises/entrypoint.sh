2ab396275a1f18146296b7640ceeec4fd4c5db91
Jacques van der Merwe <jacques.vandermerwe@swisscom.com>
2019-05-10 09:46:32 +0200

    bolting on sidecar proxy to exercises microservice

--- services/exercises/entrypoint.sh
+++ services/exercises/entrypoint.sh
@@ -10,4 +10,34 @@ echo "PostgreSQL started"
 
 python manage.py recreate-db
 python manage.py seed-db
-gunicorn -b 0.0.0.0:5000 manage:app
+nohup gunicorn -b 0.0.0.0:5000 manage:app &
+
+# consul service registration configuration
+IPADDR=`ip a show eth0 | grep -oP 'inet \K[\d\.]+'`
+
+# define local gunicorn service
+cat <<EOF | tee /etc/consul.d/${CONSUL_SERVICE}.json
+{
+  "service": {
+    "name": "${CONSUL_SERVICE}",
+    "port": 5000,
+    "connect": {
+      "sidecar_service": {}
+    }
+  }
+}
+EOF
+
+# start consul agent
+nohup consul agent \
+	-bind=${IPADDR} \
+	-retry-join=consul \
+	--data-dir=/var/consul \
+	-config-dir=/etc/consul.d \
+  &
+
+sleep 2
+
+# start consul connect proxy
+nohup consul connect proxy -sidecar-for ${CONSUL_SERVICE} &
+consul connect proxy -service ${CONSUL_SERVICE} -upstream users:5001
--- services/users/entrypoint.sh
+++ services/users/entrypoint.sh
@@ -9,14 +9,22 @@ done
 
 echo "PostgreSQL started"
 
+python manage.py recreate-db
+python manage.py seed-db
+nohup gunicorn -b 0.0.0.0:5000 manage:app &
+
 # consul service registration configuration
 IPADDR=`ip a show eth0 | grep -oP 'inet \K[\d\.]+'`
 
+# define local gunicorn service
 cat <<EOF | tee /etc/consul.d/${CONSUL_SERVICE}.json
 {
   "service": {
     "name": "${CONSUL_SERVICE}",
-    "port": 5000
+    "port": 5000,
+    "connect": {
+      "sidecar_service": {}
+    }
   }
 }
 EOF
@@ -27,11 +35,9 @@ nohup consul agent \
 	-retry-join=consul \
 	--data-dir=/var/consul \
 	-config-dir=/etc/consul.d \
-	&
+  &
 
 sleep 2
 
-# testdriven service 
-python manage.py recreate-db
-python manage.py seed-db
-gunicorn -b 0.0.0.0:5000 manage:app
+# start consul connect proxy
+consul connect proxy -sidecar-for ${CONSUL_SERVICE}
